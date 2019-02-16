1c4f27c57397a4c7026b15a433a3ed932842049a
Anton Cbtk <sender4k@gmail.com>
2019-02-16 19:16:53 +0200

    page start

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-// (function () {
-//     const menu = document.getElementById('menu')
-//     const openMenu = document.getElementById('openMenu')
-//
-//     menu.addEventListener('click', function () {
-//         openMenu.classList.toggle('active')
-//     })
-// })()
+(function () {
+    const menu = document.getElementById('menu')
+    const openMenu = document.getElementById('openMenu')
+
+    menu.addEventListener('click', function () {
+        openMenu.classList.toggle('active')
+        // openMenu.classList.toggle('height')
+    })
+})()
