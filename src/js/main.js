f4feb5ec39864390ad0768418290056f9386d617
Anton Cbtk <sender4k@gmail.com>
2019-02-21 15:55:18 +0200

    Legend End

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,33 @@
     menu.addEventListener('click', function () {
         openMenu.classList.toggle('active')
         // openMenu.classList.toggle('height')
+        // openMenu.style.top = '100%'
+    })
+})();
+(function () {
+    const form = document.getElementById('form')
+    const name = document.getElementById('name')
+    const email = document.getElementById('email')
+    const text = document.getElementById('text')
+    const btn = document.getElementById('sub')
+    const accord = document.getElementById('accordion')
+    const out = document.getElementById('out')
+    const afirm = document.getElementById('afirm')
+    const str = ''
+
+    btn.addEventListener('click', function () {
+        out.innerHTML = 'Name: ' + str + name.value + '<br>'+ 'Email: ' + str + email.value + '<br>'+ 'Text: ' + str + text.value
+        form.reset();
+        afirm.style.display = 'block'
+    })
+    accord.addEventListener('click', function () {
+        if(out.style.display == 'block'){
+            accord.innerHTML = 'Open'
+            out.style.display = 'none'
+        } else {
+            accord.innerHTML = 'Close'
+            out.style.display = 'block'
+        }
+
     })
 })()
